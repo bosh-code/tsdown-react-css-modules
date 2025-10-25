9e790bcc4b83bae5ae377c5c9ff4f5b31b1f442a
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-26 10:09:07 +1300

    feat(css-modules): add lightningcss

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,27 @@
 import { defineConfig } from 'tsdown'
+import LightningCSS from 'unplugin-lightningcss/rolldown'
 
 export default defineConfig({
-  // ...config options
+  target: [
+    'android141',
+    'chrome109',
+    'edge139',
+    'firefox140',
+    'safari18.5'
+  ],
+  entry: [
+    './src/index.ts'
+  ],
+  platform: 'browser',
+  sourcemap: true,
+  tsconfig: './tsconfig.json',
+  hash: true,
+  dts: true,
+  plugins: [
+    LightningCSS({
+      options: {
+        minify: true
+      }
+    })
+  ]
 })
